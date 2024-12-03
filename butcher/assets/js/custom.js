ec1c8006c90ebca640cfcca85c41d55596082d76
mdbakul <mdbakul51@gmail.com>
2024-12-03 14:13:35 +0600

    master

--- butcher/assets/js/custom.js
+++ butcher/assets/js/custom.js
@@ -184,27 +184,18 @@ $(function () {
         slidesToScroll: 4,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1200,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4
+                    slidesToScroll: 3
                 }
             },
 
             {
                 breakpoint: 576,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
+                    slidesToShow: 2,
+                    slidesToScroll: 2
                 }
             }
         ]
